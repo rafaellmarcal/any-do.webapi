a78ff13de8920c84188c1454d600ce01073a2b08
Rafael Marçal <36934740+rafaellmarcal@users.noreply.github.com>
2018-08-27 09:33:34 -0400

    Scripts de criação das tabelas Tarefa e Usuário.

---
+++ Database/dbo.Tarefa.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Tarefa] (
+    [TarefaID]      INT            IDENTITY (1, 1) NOT NULL,
+    [Titulo]        NVARCHAR (50)  NOT NULL,
+    [Descricao]     NVARCHAR (MAX) NOT NULL,
+    [DataCadastro]  DATETIME       NOT NULL,
+    [DataConclusao] DATETIME       NULL,
+    [Concluida]     BIT            DEFAULT ((0)) NOT NULL,
+    [UsuarioID]     INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([TarefaID] ASC),
+    CONSTRAINT [FK_Usuario_Tarefa] FOREIGN KEY ([UsuarioID]) REFERENCES [dbo].[Usuario] ([UsuarioID])
+);
+
---
+++ Database/dbo.Usuario.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Usuario] (
+    [UsuarioID] INT            IDENTITY (1, 1) NOT NULL,
+    [Nome]      NVARCHAR (255) NOT NULL,
+    [Senha]     NVARCHAR (255) NOT NULL,
+    [Email]     NVARCHAR (225) NOT NULL,
+    PRIMARY KEY CLUSTERED ([UsuarioID] ASC)
+);
+
